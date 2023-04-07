84c06ad8d99c4bc9b3af2f2bc8426b7b94b70df6
zhengjian <zhengjian@xiaohongshu.com>
2023-04-07 21:42:57 +0800

    s

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,13 @@ dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 cd $dist_path
 
 git init
+git checkout -b master
 git config --global user.email "2290653824@qq.com"
 git config --global user.name "zhengjian"
 git add -A

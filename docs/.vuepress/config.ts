92d57f3e574e5c74d8304a65b89a7c766fa4a234
BabyJIAN <2290653824@qq.com>
2023-06-22 15:15:59 +0800

    blog update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -18,8 +18,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   locales: {
     '/': {
       lang: 'zh-CN',
-      title: "Evan's blog",
-      description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
+      title: "baby sword‘s blog",
+      description: 'java后端技术总结，生活记录。个人笔记',
     }
   },
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
@@ -113,7 +113,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       { text: '其他', link: '/other/', items: [{ text: '逻辑题', items: [] }, { text: '面试', items: [{ text: '百度后端实习二面', link: '' }] }] },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png', // 导航栏logo
+    logo: '/img/favicon.ico', // 导航栏logo
     repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
